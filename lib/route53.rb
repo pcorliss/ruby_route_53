e6d73e35df7af00278680e3308b6c73cb2f82ccb
sawanoboly <sawanoboriyu@higanworks.com>
2013-08-01 16:35:41 +0900

    add option -k --ssl_no_verify for cli

--- lib/route53.rb
+++ lib/route53.rb
@@ -12,22 +12,23 @@ require 'builder'
 require 'digest/md5'
 
 module Route53
-  
+
   class Connection
     attr_reader :base_url
     attr_reader :api
     attr_reader :endpoint
     attr_reader :verbose
-    
-    def initialize(accesskey,secret,api='2011-05-05',endpoint='https://route53.amazonaws.com/',verbose=false)
+
+    def initialize(accesskey,secret,api='2011-05-05',endpoint='https://route53.amazonaws.com/',verbose=false,ssl_no_verify=false)
       @accesskey = accesskey
       @secret = secret
       @api = api
       @endpoint = endpoint
       @base_url = endpoint+@api
       @verbose = verbose
+      @ssl_no_verify = ssl_no_verify
     end
-    
+
     def request(url,type = "GET",data = nil)
       puts "URL: #{url}" if @verbose
       puts "Type: #{type}" if @verbose
@@ -35,7 +36,7 @@ module Route53
       uri = URI(url)
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true if uri.scheme == "https"
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE if RUBY_VERSION.start_with?("1.8")
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE if RUBY_VERSION.start_with?("1.8") or @ssl_no_verify
       time = get_date
       hmac = HMAC::SHA256.new(@secret)
       hmac.update(time)
@@ -50,7 +51,7 @@ module Route53
       #puts "RespBody: #{resp.body}" if @verbose
       return AWSResponse.new(resp.body,self)
     end
-    
+
     def get_zones(name = nil)
       truncated = true
       query = []
@@ -75,7 +76,7 @@ module Route53
       end
       unless name.nil? || name.start_with?("/hostedzone/")
         name_arr = name.split('.')
-        (0 ... name_arr.size).each do |i| 
+        (0 ... name_arr.size).each do |i|
           search_domain = name_arr.last(name_arr.size-i).join('.')+"."
           zone_select = zones.select { |z| z.name == search_domain }
           return zone_select
@@ -84,7 +85,7 @@ module Route53
       end
       return zones
     end
-    
+
     def get_date
       #return Time.now.utc.rfc2822
       #Cache date for 30 seconds to reduce extra calls
@@ -92,7 +93,7 @@ module Route53
         uri = URI(@endpoint)
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true if uri.scheme == "https"
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE if RUBY_VERSION.start_with?("1.8")
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE if RUBY_VERSION.start_with?("1.8") or @ssl_no_verify
         resp = nil
         puts "Making Date Request" if @verbose
         http.start { |http| resp = http.head('/date') }
@@ -102,16 +103,16 @@ module Route53
       end
       return @date
     end
-    
+
   end
-  
-  
+
+
   class Zone
     attr_reader :host_url
     attr_reader :name
     attr_reader :records
     attr_reader :conn
-    
+
     def initialize(name,host_url,conn)
       @name = name
       unless @name.end_with?(".")
@@ -120,11 +121,11 @@ module Route53
       @host_url = host_url
       @conn = conn
     end
-    
+
     def delete_zone
       @conn.request(@conn.base_url + @host_url,"DELETE")
     end
-    
+
     def create_zone(comment = nil)
       xml_str = ""
       xml = Builder::XmlMarkup.new(:target=>xml_str, :indent=>2)
@@ -132,7 +133,7 @@ module Route53
       xml.CreateHostedZoneRequest(:xmlns => @conn.endpoint+'doc/'+@conn.api+'/') { |create|
         create.Name(@name)
         # AWS lists this as required
-        # "unique string that identifies the request and that 
+        # "unique string that identifies the request and that
         # allows failed CreateHostedZone requests to be retried without the risk of executing the operation twice."
         # Just going to pass a random string instead.
         create.CallerReference(rand(2**32).to_s(16))
@@ -146,10 +147,10 @@ module Route53
       @host_url = resp_xml.search("HostedZone").first.search("Id").first.innerText
       return resp
     end
-    
+
     def get_records(type="ANY")
       return nil if host_url.nil?
-      
+
       truncated = true
       query = []
       dom_records = []
@@ -183,7 +184,7 @@ module Route53
                         (ident_records.first.innerText unless ident_records.empty?)
                         ))
         end
-        
+
         truncated = (zone_file.search("IsTruncated").first.innerText == "true")
         if truncated
           next_name = zone_file.search("NextRecordName").first.innerText
@@ -197,7 +198,7 @@ module Route53
       end
       return dom_records
     end
-    
+
     #When deleting a record an optional value is available to specify just a single value within a recordset like an MX record
     #Takes an array of [:action => , :record => ] where action is either CREATE or DELETE and record is a DNSRecord
     def gen_change_xml(change_list,comment=nil)
@@ -219,25 +220,25 @@ module Route53
       #puts "XML:\n#{xml_str}" if @conn.verbose
       return xml_str
     end
-    
+
     #For modifying multiple or single records within a single transaction
     def perform_actions(change_list,comment=nil)
       xml_str = gen_change_xml(change_list,comment)
       @conn.request(@conn.base_url + @host_url+"/rrset","POST",xml_str)
     end
 
-    
+
     def to_s
       return "#{@name} #{@host_url}"
     end
   end
-  
+
   class AWSResponse
     attr_reader :raw_data
-    
+
     #I wanted to put this in a seprate file but ruby's method of determinign the root of the gem is a pain in the butt and I was in a hurry. Sorry. -PC
 
-      
+
     def initialize(resp,conn)
       @raw_data = unescape(resp)
       if error?
@@ -252,18 +253,18 @@ module Route53
       @created = Time.now
       puts "Raw: #{@raw_data}" if @conn.verbose
     end
-    
+
     def error?
       return Hpricot::XML(@raw_data).search("ErrorResponse").size > 0
     end
-    
+
     def error_message
       xml = Hpricot::XML(@raw_data)
       msg_code = xml.search("Code")
       msg_text = xml.search("Message")
       return (msg_code.size > 0 ? msg_code.first.inner_text : "") + (msg_text.size > 0 ? ': ' + msg_text.first.innerText : "")
     end
-    
+
     def helpful_message
       xml = Hpricot::XML(@raw_data)
       msg_code = xml.search("Code").first.innerText
@@ -291,21 +292,21 @@ module Route53
       end
       return @complete
     end
-    
+
     def pending?
       #Return opposite of complete via XOR
       return complete? ^ true
     end
-    
+
     def to_s
       return @raw_data
     end
-    
+
     def unescape(string)
       string.gsub(/\\0(\d{2})/) { $1.oct.chr }
     end
   end
-  
+
   class DNSRecord
     attr_reader :name
     attr_reader :type
@@ -314,7 +315,7 @@ module Route53
     attr_reader :weight
     attr_reader :ident
     attr_reader :zone_apex
-    
+
     def initialize(name,type,ttl,values,zone,zone_apex=nil,weight=nil,ident=nil)
       @name = name
       unless @name.end_with?(".")
@@ -328,7 +329,7 @@ module Route53
       @weight = weight
       @ident = ident
     end
-    
+
     def gen_change_xml(xml,action)
       xml.Change { |change|
         change.Action(action.upcase)
@@ -355,15 +356,15 @@ module Route53
         }
       }
     end
-    
+
     def delete(comment=nil)
       @zone.perform_actions([{:action => "DELETE", :record => self}],comment)
     end
-    
+
     def create(comment=nil)
       @zone.perform_actions([{:action => "CREATE", :record => self}],comment)
     end
-    
+
     #Need to modify to a param hash
     def update(name,type,ttl,values,comment=nil, zone_apex = nil)
       prev = self.clone
@@ -377,7 +378,7 @@ module Route53
           {:action => "CREATE", :record => self},
           ],comment)
     end
-    
+
     #Returns the raw array so the developer can update large batches manually
     #Need to modify to a param hash
     def update_dirty(name,type,ttl,values,zone_apex = nil)
@@ -390,7 +391,7 @@ module Route53
       return [{:action => "DELETE", :record => prev},
       {:action => "CREATE", :record => self}]
     end
-    
+
     def to_s
       if @weight
         "#{@name} #{@type} #{@ttl} '#{@ident}' #{@weight} #{@values.join(",")}"
@@ -412,5 +413,5 @@ end
                   "HostedZoneNotEmpty" => "You'll need to first delete the contents of this zone. You can do so using the '--remove' option as part of the command line interface.",
                   "InvalidChangeBatch" => "You may have tried to delete a NS or SOA record. This error is safe to ignore if you're just trying to delete all records as part of a zone prior to deleting the zone. Or you may have tried to create a record that already exists. Otherwise please file a bug by sending a detailed bug report with the entire input and output from the program to support@50projects.com or to https://github.com/pcorliss/ruby_route_53/issues and we'll do out best to help you.",
                   "ValidationError" => "Check over your input again to make sure the record to be created is valid. The error message should give you some hints on what went wrong. If you're still having problems please file a bug by sending a detailed bug report with the entire input and output from the program to support@50projects.com or to https://github.com/pcorliss/ruby_route_53/issues and we'll do out best to help you."}
-                  
+
 
